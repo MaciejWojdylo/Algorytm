4ee234006c9f1e78baab038b91c51ee3d170e580
Stacjonarka <mjwoj@DESKTOP-4R65IHO>
2024-10-26 21:49:51 +0200

    Dodanie flagi wczytywania z pliku tworzenie macierzyt

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
+import argparse
 sequences = []
-tmp = ""
-with open("seq1.fasta" , "r") as f:
-    for l in f:
-        if(l.startswith(">")):
-            if(tmp == ""):
-                continue
-            else:
-                sequences.append(tmp)
-                tmp = ""
-        else:
-            tmp+=l.strip().upper()
-if(tmp):
-    sequences.append(tmp)
+nameseq = []
+def main(file_path):
+    try:
+        with open("seq1.fasta", "r") as f:
+            tmp = ""
+            for l in f:
+                if l.startswith(">"):
+                    nameseq.append(l.strip().replace(">",""))
+                    if tmp == "":
+                        continue
+                    else:
+                        sequences.append(tmp)
+                        tmp = ""
+                else:
+                    tmp += l.strip().upper()
+        if tmp:
+            sequences.append(tmp)
+    except FileNotFoundError:
+        print(f"Plik {file_path} nie został znaleziony.")
 
-matrix = [[0]*len(sequences[0])]*len(sequences[1])
-print(matrix)
-print(sequences)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Program do odczytu pliku FASTA")
+    parser.add_argument("-i","--input", default="seq1.fasta" , help="Ścieżka do pliku FASTA") #required=True w miejsce default  default="seq1.fasta"
+    args = parser.parse_args()
+    main(args.input)
+rows = len(sequences[0])
+columns = len(sequences[1])
+matrix = [[0 for _ in range(columns)] for _ in range(rows+1)]
