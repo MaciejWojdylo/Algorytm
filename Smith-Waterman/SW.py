e49342f9f59ed311962fe699bb2aa916cdaef164
ania <ania.szymczak2002@gmail.com>
2024-10-27 17:01:29 +0100

    znalezienie ścieżki w macierzy zapisanie odpowiedzi do pliku

--- Smith-Waterman/SW.py
+++ Smith-Waterman/SW.py
@@ -15,6 +15,12 @@ def main(file_path):
         sequences.append(seq)
         print(sequences)
 
+def show_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end="\t")
+        print("")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Program do odczytu pliku FASTA")
@@ -30,4 +36,66 @@ match = args.match
 missmatch = args.missmatch
 rows=len(sequences[0])
 columns=len(sequences[1])
+matrix = [[0 for _ in range(columns+1)] for _ in range(rows+1)]
+maximum=0
+positionX=0
+positionY=0
+for i in range(1,rows+1):
+    for j in range(1,columns+1):
+        up=matrix[i-1][j] + gap
+        left=matrix[i][j-1] + gap
+        if sequences[0][i-1]==sequences[1][j-1]:
+            diagonal = matrix[i - 1][j - 1] + match
+        else:
+            diagonal = matrix[i - 1][j - 1] + missmatch
+        if max(up, left, diagonal) <= 0:
+            matrix[i][j] = 0
+        else:
+            matrix[i][j] = max(up, left, diagonal)
+        if matrix[i][j] > maximum:
+            maximum = matrix[i][j]
+            positionX = i
+            positionY = j
+seq1=""
+seq2=""
+while matrix[positionX][positionY] != 0:
+    up=matrix[positionX-1][positionY] + gap
+    left=matrix[positionX][positionY-1] + gap
+    if sequences[0][positionX-1]==sequences[1][positionY-1]:
+        diagonal = matrix[positionX-1][positionY-1] + match
+    else:
+        diagonal = matrix[positionX-1][positionY-1] + missmatch
+    if up < 0:
+        up=0
+    if left < 0:
+        left=0
+    if diagonal < 0:
+        diagonal=0
+    if matrix[positionX][positionY] == up:
+        seq1=seq1+sequences[0][positionX-1]
+        seq2=seq2+"-"
+        positionX=positionX-1
+        continue
+    if matrix[positionX][positionY] == left:
+        seq1=seq1+"-"
+        seq2=seq2+sequences[1][positionY-1]
+        positionY=positionY-1
+        continue
+    if matrix[positionX][positionY] == diagonal:
+        seq1=seq1+sequences[0][positionX-1]
+        seq2=seq2+sequences[1][positionY-1]
+        positionX=positionX-1
+        positionY=positionY-1
+        continue
+show_matrix(matrix)
+
+with open("result.txt", "w") as file:
+    file.write(seq1[::-1])
+    file.write("\n")
+    file.write(seq2[::-1])
+
+
+
+
+
 
